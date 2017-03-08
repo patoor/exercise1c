b48179adc12d415515b35f1345c6a7bd5191af7e
patoor <pator0509@wp.pl>
2017-03-08 18:31:41 +0100

    homework

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -1,12 +1,21 @@
 package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	private static final Logger log = LogManager.getLogger();
     public int[] convertToInts(String[] strings) {
+		log.debug("Started :" );
+		log.debug("convertToInts", Arrays.toString(strings));
+		log.debug("Ended :" );
+        log.debug("convertToInts", Arrays.stream(strings).mapToInt(Integer::valueOf).toArray());
         return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+		
+
     }
 }
